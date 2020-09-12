665917622cca3d506a7df3512d1361b060e3d48f
noguchi <noguchi.hnut@gmail.com>
2020-09-12 18:39:14 +0900

    get json data.

--- Swift5Bokete1/ViewController.swift
+++ Swift5Bokete1/ViewController.swift
@@ -18,12 +18,71 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var odaiImageView: UIImageView!
     
+    @IBOutlet weak var commentTextView: UITextView!
+    
+    @IBOutlet weak var searchTextView: UITextField!
+    
+    var count = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        commentTextView.layer.cornerRadius = 20.0
+        
+        PHPhotoLibrary.requestAuthorization{ (status) in
+        
+        switch(status) {
+            case .authorized: break
+            case .denied: break
+            case .notDetermined: break
+            case .restricted: break
+            
+            
+        }
     }
 
+    }
+
+    //検索キーワード
+    //pixabay.com
+    
+    func getImages(keyword:String){
+        
+        //APIkey
+        let url = "https://pixabay.com/api/?key=18277393-2d13279decb5bf1144e6a304b&q=\(keyword)"
+        
+        //Alamofireを使ってhttpリクエストを投げる。
+        AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
+            
+            switch response.result{
+                
+            case .success:
+                let json:JSON = JSON(response.data as Any)
+                let imageString = json["hits"][self.count]["webformatURL"].string
+                self.odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
+            
+            
+            
+            case .failure(let error):
+                
+                  print(error)
+            
+                
+            }
+            
+        }
+
+    }
+  
+    
+    @IBAction func nextOdai(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
+    
 
 }
 
