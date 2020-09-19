84aa95fb3021a7f28ca1bcefb8f42e894a1f8de4
noguchi <noguchi.hnut@gmail.com>
2020-09-19 12:55:29 +0900

    create shareViewController.

--- Swift5Bokete1/ViewController.swift
+++ Swift5Bokete1/ViewController.swift
@@ -37,6 +37,7 @@ class ViewController: UIViewController {
             case .notDetermined: break
             case .restricted: break
             
+        default:break
             
         }
     }
@@ -58,14 +59,28 @@ class ViewController: UIViewController {
         let url = "https://pixabay.com/api/?key=18277393-2d13279decb5bf1144e6a304b&q=\(keyword)"
         
         //Alamofireを使ってhttpリクエストを投げる。
+        //値が返ってきて、それをJSON解析を行う
+        //imageview.imageに貼り付ける
         AF.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default).responseJSON { (response) in
             
             switch response.result{
                 
             case .success:
                 let json:JSON = JSON(response.data as Any)
-                let imageString = json["hits"][self.count]["webformatURL"].string
-                self.odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
+                var imageString = json["hits"][self.count]["webformatURL"].string
+                
+                if imageString == nil{
+                    
+                    imageString = json["hits"][0]["webformatURL"].string
+                    self.odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
+
+                }else{
+                    
+                    self.odaiImageView.sd_setImage(with: URL(string: imageString!), completed: nil)
+                }
+                
+                
+                
             
             
             
@@ -84,10 +99,48 @@ class ViewController: UIViewController {
     @IBAction func nextOdai(_ sender: Any) {
         
         count = count + 1
-        // getImages
+        
+        if searchTextView.text == ""{
+            
+            getImages(keyword : "funny")
+            
+        }else{
+            
+            getImages(keyword: searchTextView.text!)
+            
+        }
         
     }
     
+    @IBAction func searchAction(_ sender: Any) {
+        
+        self.count = 0
+        if searchTextView.text == ""{
+            
+            getImages(keyword : "funny")
+            
+        }else{
+            
+            getImages(keyword: searchTextView.text!)
+            
+        }
+        
+    }
+    
+    
+    @IBAction func next(_ sender: Any) {
+        
+        performSegue(withIdentifier: "next", sender: nil)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let shareVC = segue.destination as? ShareViewController
+        
+        shareVC?.commentString = commentTextView.text
+        shareVC?.resultImage = odaiImageView.image!
+        
+    }
     
     
 
